c5dd91051f01bea4351146616d4d4476d5568976
karimadnan <karimadnan@>
2021-10-14 22:02:37 +0200

    init

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, } from '@react-navigation/native-stack';
+import ChatRoom from './screens/chatRoom';
+import Landing from './screens/landing';
+import InterestsHandler from './components/interestsHandler';
+import SocketHandler from './components/socketHandler';
+import Settings from './components/settings';
+import { colors } from './lib/constants';
 
-export default function App() {
+const App = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.root}>
+
+      <StatusBar
+        animated={true}
+        backgroundColor={colors.statusBar}
+        barStyle={'light-content'}
+      />
+      <SocketHandler />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          headerShown: false,
+          animation: 'slide_from_right',
+          gestureEnabled: true
+        }}>
+          <Stack.Screen name="Landing" component={Landing} />
+          <Stack.Screen name="InterestsHandler" component={InterestsHandler} />
+          <Stack.Screen name="Settings" component={Settings} />
+          <Stack.Screen name="Chat" component={ChatRoom} />
+        </Stack.Navigator>
+      </NavigationContainer>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  }
 });
+export default App;
